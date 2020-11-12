b09d77a883d94b769c699fb59e3b69d184d0e361
chadleslie <74363865+chadleslie@users.noreply.github.com>
2020-11-12 08:15:34 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,2 +1,2 @@
-var cookies = document.cookie
-print(cookies)
+let c = document.cookie.split(";").reduce( (ac, cv, i) => Object.assign(ac, {[cv.split('=')[0]]: cv.split('=')[1]}), {});
+console.log(c);
