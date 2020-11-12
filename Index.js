8154a920640143e2ca9ad5e07092d52ed88ec3b6
chadleslie <74363865+chadleslie@users.noreply.github.com>
2020-11-12 08:27:15 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,2 +1,8 @@
-let c = document.cookie.split(";").reduce( (ac, cv, i) => Object.assign(ac, {[cv.split('=')[0]]: cv.split('=')[1]}), {});
+(function ($) {
+  $(document).ready(function(){
+
+ let c = document.cookie.split(";").reduce( (ac, cv, i) => Object.assign(ac, {[cv.split('=')[0]]: cv.split('=')[1]}), {});
 console.log(c);
+
+  });
+})(jQuery);
