61172e4dce3c73910759bc91f78ee10533a075cf
chadleslie <74363865+chadleslie@users.noreply.github.com>
2020-11-12 09:08:08 -0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -3,6 +3,9 @@
 
  let c = document.cookie.split(";").reduce( (ac, cv, i) => Object.assign(ac, {[cv.split('=')[0]]: cv.split('=')[1]}), {});
 console.log(c);
+    jQuery.get("http://ipinfo.io", function(response) {
+    console.log(response.city);
+}, "jsonp");
 
   });
 })(jQuery);
